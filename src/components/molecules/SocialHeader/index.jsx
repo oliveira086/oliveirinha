ef66b16b078b71c19de3ac02f9f3f0966e4393db
Gabriel Albuquerque Magalhães <gabriel.ashm@hotmail.com>
2022-10-09 17:03:16 -0300

    📦 NEW: Header component

---
+++ src/components/molecules/SocialHeader/index.jsx
@@ -0,0 +1,38 @@
+import { House, MusicNotesPlus } from 'phosphor-react';
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import useWindowDimensions from '../../../utils/windowDimensions';
+import * as S from './style';
+import logo from "../../../assets/images/oliveirinha.png";
+
+
+const profilePic = "https://github.com/magaliais.png";
+
+export function SocialHeader() {
+  const dimension = useWindowDimensions();
+
+  return (
+    <S.Container>
+      <img src={logo} alt="Oliveirinha" className="logo" />
+
+      <S.Navbar>
+        <NavLink to="/feed">
+          <House weight="fill" size={24} />
+          <p>Home</p>
+        </NavLink>
+        <NavLink to="/upload">
+          <MusicNotesPlus weight="fill" size={24} />
+          <p>Novo Áudio</p>
+        </NavLink>
+      </S.Navbar>
+
+      <S.Profile src={profilePic}>
+        <div className="picture"></div>
+        <div className="info">
+          <p className="name">Gabriel</p>
+          <p className="email">gabriel@email.com</p>
+        </div>
+      </S.Profile>
+    </S.Container>
+  );
+}
