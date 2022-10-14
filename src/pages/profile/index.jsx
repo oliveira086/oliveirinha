5fa75782d7bc9c42774a3b3da6683b4a58d71c85
Gabriel Albuquerque Magalhães <gabriel.ashm@hotmail.com>
2022-10-14 16:23:49 -0300

    📦 NEW: profile page

---
+++ src/pages/profile/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import * as S from './style'
+
+export function Profile() {
+  return (
+    <S.Container>
+      <S.Header>
+        
+      </S.Header>
+
+      <S.Content>
+
+      </S.Content>
+    </S.Container>
+  )
+}
